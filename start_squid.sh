0ef0f335ac0564417a86b21604976bfa7dac8134
aidart <venescu70@gmail.com>
2021-12-01 21:06:23 +0100

    Update start_squid.sh

--- start_squid.sh
+++ start_squid.sh
@@ -1,1 +1,4 @@
 docker build -t dockerworx/docker-squid .
+docker run -d --name="docker-squid" \
+            -e SQUID_USERNAME=enescva -e SQUID_PASSWORD=efsa \
+            -p 3129:3128 dockerworx/docker-squid
