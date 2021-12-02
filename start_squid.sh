1a4ee2724c90fc29da12c899307383c05bf91f77
aidart <venescu70@gmail.com>
2021-12-02 20:42:15 +0100

    Delete start_squid.sh

--- start_squid.sh
+++
@@ -1,4 +0,0 @@
-docker build -t dockerworx/docker-squid .
-docker run -d --name="docker-squid" \
-            -e SQUID_USERNAME=user -e SQUID_PASSWORD=efsa \
-            -p 3129:3128 dockerworx/docker-squid
