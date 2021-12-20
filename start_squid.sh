d562bf73ec08f3013eaa9402f07e99dc9ffa3ed9
enescva <87376170+enescva@users.noreply.github.com>
2021-12-20 14:18:51 +0100

    Update start_squid.sh

--- start_squid.sh
+++ start_squid.sh
@@ -1,4 +1,4 @@
 docker build -t dockerworx/docker-squid .
 docker run -d --name="docker-squid" \
-            -e SQUID_USERNAME=user -e SQUID_PASSWORD=proxy \
+            -e SQUID_USERNAME=username -e SQUID_PASSWORD=proxypass \
             -p 3129:3128 dockerworx/docker-squid
